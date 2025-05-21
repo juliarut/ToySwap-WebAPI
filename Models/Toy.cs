8da299ff3f963688fd928f0e72be266bdf88ff80
juliarut <julia.rutfjall@gmail.com>
2025-05-21 18:57:54 +0200

    Add Toy model

---
+++ Models/Toy.cs
@@ -0,0 +1,12 @@
+namespace ToySwap.Models
+{
+    public class Toy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Condition { get; set; }
+        public int MinAge { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
